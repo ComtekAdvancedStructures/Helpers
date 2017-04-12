98da79fe5aabe00b6c9d03ec5c41b1f1f65f1ff6
cverburgh <cverburgh@outlook.com>
2017-04-12 12:55:11 -0400

    Added a DTO for the JQuery UI autocomplete function

---
+++ AutoCompleteDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Comtek.Helpers
+{
+    /// <summary>
+    ///     An object to use when returning results from a JQuery UI Autocomplete lookup
+    /// </summary>
+    public class AutoCompleteDto
+    {
+        /// <summary>
+        ///     What is shown to the user in the drop down list and what populates the text box once selected
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        ///     This can be used as an Id
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        ///     Extra information about the item returned
+        /// </summary>
+        public string Desc { get; set; }
+    }
+}
